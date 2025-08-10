b396411343e69c29b5357385ebbf7625dcc37618
boki <lsb530@naver.com>
2025-08-10 23:48:18 +0900

    maru nopower img, apply different loading animation each Platform

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -1,16 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 플랫폼에 따라 다른 앱 위젯 사용
+    if (Platform.isIOS) {
+      return CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      );
+    }
+  }
+}
+
 // stless: StatelessWidget
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen2 extends StatelessWidget {
+  const HomeScreen2({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,17 +46,74 @@ class HomeScreen extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: 32.0,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'asset/img/logo.png',
-            ),
-            SizedBox(height: 28.0),
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                width: 200.0,
+                'asset/img/nopower.png',
+              ),
+              SizedBox(height: 28.0),
+              CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 현재 플랫폼 확인
+    String platformText = Platform.isIOS ? 'iOS' : 'Android';
+
+    return Scaffold(
+      backgroundColor: Color(0xFF335CB0),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 32.0,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                platformText,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Image.asset(
+                width: 200.0,
+                'asset/img/nopower.png',
+              ),
+              SizedBox(height: 28.0),
+              Platform.isIOS ? CupertinoActivityIndicator(
+                color: Colors.white,
+                radius: 14.0,
+              ) : CircularProgressIndicator(
+                color: Colors.white,
+              ),
+              SizedBox(height: 28.0),
+              Text(
+                "made by boki",
+                style: TextStyle(
+                  color: Colors.greenAccent,
+                  fontSize: 25.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
