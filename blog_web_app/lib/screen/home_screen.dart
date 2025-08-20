f5768b9f721c9b9173b2326607c84f3708ddf319
boki <lsb530@naver.com>
2025-08-20 21:36:16 +0900

    change JavascriptMode

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -7,7 +7,10 @@ class HomeScreen extends StatelessWidget {
   // ..: 함수를 실행한 결과값이 아니라, 함수가 실행된 대상이 반환된다.(아래와 동일)
   // final controller = WebViewController();
   // controller.loadRequest();
-  WebViewController controller = WebViewController()..loadRequest(homeUrl);
+  WebViewController controller = WebViewController()
+    // ..setJavaScriptMode(JavaScriptMode.disabled)
+    ..setJavaScriptMode(JavaScriptMode.unrestricted) // Youtube For Android
+    ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
