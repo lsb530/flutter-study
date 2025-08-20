e17be31f79d3adbecabc76d58b8669938623ec48
boki <lsb530@naver.com>
2025-08-20 21:20:25 +0900

    website on WebView

--- blog_web_app/lib/main.dart
+++ blog_web_app/lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:blog_web_app/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프레임워크가 실행할 준비가 될 때까지 기다린다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
-        home: HomeScreen()
+      home: HomeScreen(),
     ),
   );
 }
--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  // ..: 함수를 실행한 결과값이 아니라, 함수가 실행된 대상이 반환된다.(아래와 동일)
+  // final controller = WebViewController();
+  // controller.loadRequest();
+  WebViewController controller = WebViewController()..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +17,16 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
         title: Text(
-            'Code Boki',
+          'Code Boki',
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
+      ),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
