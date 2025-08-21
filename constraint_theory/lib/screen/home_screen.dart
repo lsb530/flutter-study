d48e76eb43486a88599337f8d85db548015a4879
boki <lsb530@naver.com>
2025-08-22 08:47:49 +0900

    constraint

--- constraint_theory/lib/screen/home_screen.dart
+++ constraint_theory/lib/screen/home_screen.dart
@@ -6,7 +6,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: Container( // Center가 없으면 좌상단에 위치
+          height: 200,
+          width: 200,
+          color: Colors.red,
+          child: Container( // 전체 영역 차지
+            height: 50,
+            width: 50,
+            color: Colors.blue,
+          )
+          // child: Row(
+          //   children: [
+          //     Container(
+          //       height: 50,
+          //       width: 50,
+          //       color: Colors.blue,
+          //     ),
+          //   ],
+          // ),
+        ),
+      ),
     );
   }
 }
