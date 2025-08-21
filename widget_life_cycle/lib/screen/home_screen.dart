7fe1ce94d2f14044b66849f6bf2208e2ab05ee39
boki <lsb530@naver.com>
2025-08-21 15:01:15 +0900

    lifecycle - StatefulWidget

--- widget_life_cycle/lib/screen/home_screen.dart
+++ widget_life_cycle/lib/screen/home_screen.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key}) {
-    print('--- HomeScreen 생성자 ---');
-  }
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool show = false;
 
   @override
   Widget build(BuildContext context) {
-    print('--- HomeScreen 빌드 ---');
 
     return Scaffold(
-      body: BokiFactoryWidget(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (show) BokiFactoryWidget(), // Only can for []
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                // 외부에도 가능은 하지만 명시적으로 내부에 코드 추천
+                setState(() {
+                  show = !show;
+                });
+              },
+              child: Text('클릭해서 보이기/안보이기'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class BokiFactoryWidget extends StatelessWidget {
-  BokiFactoryWidget({super.key}) {
-    print('--- BokiFactoryWidget 생성자 ---');
-  }
+  const BokiFactoryWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('--- BokiFactoryWidget 빌드 ---');
 
     return Container(
       width: 50.0,
