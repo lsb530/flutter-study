e69bd8b8e253283fc9f78c764978bdbe47ac014e
boki <lsb530@naver.com>
2025-08-21 20:53:10 +0900

    add controller, timer

--- image_carousel/lib/widget/home_screen.dart
+++ image_carousel/lib/widget/home_screen.dart
@@ -1,19 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(
+      Duration(seconds: 2),
+      (timer) {
+        int currentPage = controller.page!.toInt();
+        int nextPage = currentPage + 1;
+
+        if (nextPage > 4) {
+          nextPage = 0;
+        }
+
+        controller.animateToPage(
+          nextPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.linear,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        children: [1,2,3,4,5].map((e) =>
-          Image.asset(
-            'asset/img/image_$e.jpeg',
-            fit: BoxFit.cover,
-          ),
-        ).toList(),
+        controller: controller,
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (e) => Image.asset(
+                'asset/img/image_$e.jpeg',
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // Memory Leak 방지
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
   }
 }
