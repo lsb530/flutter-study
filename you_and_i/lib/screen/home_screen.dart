e81d9f91c0364530a32a17a21de12574940431d7
boki <lsb530@naver.com>
2025-08-22 00:01:29 +0900

    divide style Widgets

--- you_and_i/lib/screen/home_screen.dart
+++ you_and_i/lib/screen/home_screen.dart
@@ -14,62 +14,81 @@ class HomeScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width, // /2 /3 등을 할 수 있다.
           child: Column(
             children: [
-              Expanded(
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text(
-                        'U&I',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'parisienne',
-                          fontSize: 80.0,
-                        ),
-                      ),
-                      Text(
-                        '우리 처음 만난날',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 30.0,
-                        ),
-                      ),
-                      Text(
-                        '2022.12.25',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      IconButton(
-                        iconSize: 60.0,
-                        color: Colors.redAccent,
-                        onPressed: () {},
-                        icon: Icon(Icons.favorite),
-                      ),
-                      Text(
-                        'D+1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 50.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Image.asset(
-                    'asset/img/middle_image.png',
-                  ),
-                ),
-              ),
+              _Top(),
+              /// 이미지
+              _Bottom(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              'U&I',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'parisienne',
+                fontSize: 80.0,
+              ),
+            ),
+            Text(
+              '우리 처음 만난날',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 30.0,
+              ),
+            ),
+            Text(
+              '2022.12.25',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 20.0,
+              ),
+            ),
+            IconButton(
+              iconSize: 60.0,
+              color: Colors.redAccent,
+              onPressed: () {},
+              icon: Icon(Icons.favorite),
+            ),
+            Text(
+              'D+1',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontWeight: FontWeight.w700,
+                fontSize: 50.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset(
+          'asset/img/middle_image.png',
         ),
       ),
     );
