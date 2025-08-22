3e6d9ed41aeb6440462a285473c8556f6f970897
boki <lsb530@naver.com>
2025-08-23 02:14:47 +0900

    style with ElevatedButton

--- button_theory/lib/screen/home_screen.dart
+++ button_theory/lib/screen/home_screen.dart
@@ -12,9 +12,41 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
-            OutlinedButton(onPressed: () {}, child: Text('Outlined Button')),
-            TextButton(onPressed: () {}, child: Text('Text Button')),
+            ElevatedButton(
+              onPressed: () {},
+              // onPressed: null, // disabled
+              style: ElevatedButton.styleFrom(
+                // 배경 색상
+                backgroundColor: Colors.red,
+                disabledBackgroundColor: Colors.grey,
+                // 배경 위의 색상
+                foregroundColor: Colors.white,
+                disabledForegroundColor: Colors.red,
+                shadowColor: Colors.green,
+                elevation: 10,
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                ),
+                padding: EdgeInsets.all(32.0),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 12.0,
+                ),
+                // minimumSize: Size(300, 150),
+                // maximumSize: Size(100, 150),
+                // fixedSize: Size(100, 150),
+              ),
+              child: Text('Elevated Button'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Text Button'),
+            ),
           ],
         ),
       ),
