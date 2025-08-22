3854ccc5afcbd116f8265025299dc8342e79ab4c
boki <lsb530@naver.com>
2025-08-22 09:09:15 +0900

    change url, add running image

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final homeUrl = Uri.parse('https://blog.codefactory.ai');
+// final homeUrl = Uri.parse('https://blog.codefactory.ai');
+final homeUrl = Uri.parse('https://code-boki.tistory.com');
 
 class HomeScreen extends StatelessWidget {
   // ..: 함수를 실행한 결과값이 아니라, 함수가 실행된 대상이 반환된다.(아래와 동일)
