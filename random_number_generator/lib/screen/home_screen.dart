02dd3e9d1bf4837d9d06fd74421f9f34373f150b
boki <lsb530@naver.com>
2025-08-22 22:40:28 +0900

    layout

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -6,7 +6,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 제목 & 아이콘
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('제목'),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.settings,
+                  ),
+                ),
+              ],
+            ),
+
+            // 숫자
+            Expanded(
+              child: Text('''
+              123
+              456
+              789
+              '''),
+            ),
+
+            // 버튼
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('생성하기!'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
