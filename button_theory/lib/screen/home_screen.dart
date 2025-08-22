308100785f67fa519b388ceadf50ff342ae09955
boki <lsb530@naver.com>
2025-08-23 02:09:41 +0900

    default 3 buttons(Elevated, Outlined, Text)

---
+++ button_theory/lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
+            OutlinedButton(onPressed: () {}, child: Text('Outlined Button')),
+            TextButton(onPressed: () {}, child: Text('Text Button')),
+          ],
+        ),
+      ),
+    );
+  }
+}
