299627b3682ef17aaf6381d5ab90dd862bebd6b8
boki <lsb530@naver.com>
2025-08-22 22:45:55 +0900

    ui design

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,38 +7,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            // 제목 & 아이콘
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('제목'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // 제목 & 아이콘
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '랜덤 숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  IconButton(
+                    color: redColor,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                    ),
+                  ),
+                ],
+              ),
 
-            // 숫자
-            Expanded(
-              child: Text('''
+              // 숫자
+              Expanded(
+                child: Text('''
               123
               456
               789
               '''),
-            ),
+              ),
 
-            // 버튼
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('생성하기!'),
-            ),
-          ],
+              // 버튼
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: redColor,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {},
+                child: Text('생성하기!'),
+              ),
+            ],
+          ),
         ),
       ),
     );
