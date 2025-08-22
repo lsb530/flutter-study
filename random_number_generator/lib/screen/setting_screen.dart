663d0d825b2075c182261d0c42452d12b5556500
boki <lsb530@naver.com>
2025-08-22 23:49:54 +0900

    render maxNumber to setting screen

--- random_number_generator/lib/screen/setting_screen.dart
+++ random_number_generator/lib/screen/setting_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:random_number_generator/constant/color.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  double maxNumber = 1000;
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,68 @@ class SettingScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: redColor,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('저장!'),
-              ),
+              _Number(maxNumber: maxNumber),
+              _Slider(),
+              _Button(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Number extends StatelessWidget {
+  final double maxNumber;
+
+  const _Number({required this.maxNumber, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Row(
+          children: maxNumber
+              .toInt()
+              .toString()
+              .split('')
+              .map(
+                (number) => Image.asset(
+                  'asset/img/$number.png',
+                  width: 50.0,
+                  height: 70.0,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _Slider extends StatelessWidget {
+  const _Slider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      child: Text('저장!'),
     );
   }
 }
