2fc769c3f7b3a56ecdfd267d70e667dd1f2c282b
boki <lsb530@naver.com>
2025-08-23 02:26:50 +0900

    widgetStateProperty.all() [MaterialStateProperty - deprecated]

--- button_theory/lib/screen/home_screen.dart
+++ button_theory/lib/screen/home_screen.dart
@@ -40,11 +40,29 @@ class HomeScreen extends StatelessWidget {
               child: Text('Elevated Button'),
             ),
             OutlinedButton(
-              onPressed: () {},
+              // onPressed: () {},
+              onPressed: null,
               // ElevatedButton의 모든 속성들을 가져다 쓸 수 있다.
-              style: OutlinedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.red,
+              style: ButtonStyle(
+                // !! deprecated MaterialStateProperty
+                /// Material State
+                /// all: 아래의 전체 상태에 적용
+                ///
+                /// hovered - 호버링 상태 (마우스 커서를 올려놓은 상태)
+                /// focused - 포커스 됐을 때 (텍스트 필드)
+                /// pressed - 눌렀을 때 [o]
+                /// dragged - 드래그 됐을 때
+                /// selected - 선택 됐을 때 (체크박스, 라디오 버튼)
+                /// scrollUnder - 다른 컴포넌트 밑으로 스크롤링 됐을 때
+                /// disabled - 비활성화 됐을 때 [o]
+                /// error - 에러 상태일 때
+                // backgroundColor: MaterialStateProperty.all(
+                backgroundColor: WidgetStateProperty.all(
+                  Colors.red,
+                ),
+                minimumSize: WidgetStateProperty.all(
+                  Size(200, 150),
+                ),
               ),
               child: Text('Outlined Button'),
             ),
