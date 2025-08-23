0d9199cffa0930d0c667a71d78a239f9718736ca
boki <lsb530@naver.com>
2025-08-23 18:39:47 +0900

    design homescreen - copyWith

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -6,7 +6,58 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _Logo(),
+            SizedBox(height: 28.0),
+            _Title(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'asset/image/logo.png',
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 32.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
     );
   }
 }
