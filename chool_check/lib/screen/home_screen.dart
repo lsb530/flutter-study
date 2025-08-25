c542f1d403805c04db2c6278c369d6483d3a5a60
boki <lsb530@naver.com>
2025-08-25 14:07:48 +0900

    check Geolocation Permission func

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
   final CameraPosition initialPosition = CameraPosition(
     target: LatLng(
       37.5214,
@@ -16,6 +18,31 @@ class _HomeScreenState extends State<HomeScreen> {
     ),
     zoom: 15,
   );
+
+  @override
+  void initState() {
+    super.initState();
+    checkPermission();
+  }
+
+  checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      throw Exception('위치 기능을 활성화 해주세요.');
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+    }
+
+    if (checkedPermission != LocationPermission.always ||
+    checkedPermission != LocationPermission.whileInUse) {
+      throw Exception('위치 권한을 허가 해주세요.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
