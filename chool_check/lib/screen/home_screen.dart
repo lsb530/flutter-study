ea7cb2dec1fc84b09df3eca82e8122e63b4fd31e
boki <lsb530@naver.com>
2025-08-25 12:50:23 +0900

    show Google Maps - korea

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final CameraPosition initialPosition = CameraPosition(
+    target: LatLng(
+      37.5233273,
+      126.921252,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: initialPosition,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
