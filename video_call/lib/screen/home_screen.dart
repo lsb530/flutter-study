7872058be6da7bd1e83a182d7e12b6cf1bab43be
boki <lsb530@naver.com>
2025-08-26 02:06:52 +0900

    homeScreen UI

--- video_call/lib/screen/home_screen.dart
+++ video_call/lib/screen/home_screen.dart
@@ -6,7 +6,99 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.blue[100],
+      body: Column(
+        children: [
+          /// 1) 로고
+          Expanded(
+            child: _Logo(),
+          ),
+
+          /// 2) 이미지
+          Expanded(
+            child: _Image(),
+          ),
+
+          /// 3) 버튼
+          Expanded(
+            child: _Footer(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300]!,
+              blurRadius: 12.0,
+              spreadRadius: 2.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.videocam,
+                color: Colors.white,
+              ),
+              SizedBox(width: 12.0),
+              Text(
+                'LIVE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        'asset/img/home_img.png',
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        child: Text('입장하기'),
+      ),
     );
   }
 }
