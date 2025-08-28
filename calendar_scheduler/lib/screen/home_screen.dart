2c2d7b206a6b16be5a35cdf4885bb45cf262f320
boki <lsb530@naver.com>
2025-08-28 21:41:13 +0900

    select the day on calendar

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay;
+
+  @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final firstDayOfCurrentMonth = DateTime(now.year, now.month, 1);
+
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: TableCalendar(
+          focusedDay: firstDayOfCurrentMonth.copyWith(
+            day: now.day,
+          ),
+          firstDay: DateTime(
+            1800,
+          ),
+          lastDay: DateTime(
+            3000,
+          ),
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              this.selectedDay = selectedDay;
+            });
+          },
+          selectedDayPredicate: (day) {
+            if (selectedDay == null) {
+              return false;
+            }
+
+            return day.isAtSameMomentAs(selectedDay!);
+          },
+        ),
+      ),
     );
   }
 }
