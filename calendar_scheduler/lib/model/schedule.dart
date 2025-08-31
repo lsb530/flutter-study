942b12b4aa49f4d2966cf1fd3143c8163bff4872
boki <lsb530@naver.com>
2025-09-01 01:17:57 +0900

    create drift model

--- calendar_scheduler/lib/model/schedule.dart
+++ calendar_scheduler/lib/model/schedule.dart
@@ -1,32 +1,26 @@
-class Schedule {
+import 'package:drift/drift.dart';
+
+class Schedule extends Table {
   /// 1) 식별 가능한 ID
-  final int id;
+  IntColumn get id => integer().autoIncrement()();
 
   /// 2) 시작 시간
-  final int startTime;
+  IntColumn get startTime => integer()();
 
   /// 3) 종료 시간
-  final int endTime;
+  IntColumn get endTime => integer()();
 
   /// 4) 일정 내용
-  final String content;
+  TextColumn get content => text()();
 
   /// 5) 날짜
-  final DateTime date;
+  DateTimeColumn get date => dateTime()();
 
   /// 6) 카테고리 색상
-  final String color;
+  TextColumn get color => text()();
 
   /// 7) 일정 생성일시
-  final DateTime createdAt;
-
-  Schedule({
-    required this.id,
-    required this.startTime,
-    required this.endTime,
-    required this.content,
-    required this.date,
-    required this.color,
-    required this.createdAt,
-  });
+  DateTimeColumn get createdAt => dateTime().clientDefault(
+    () => DateTime.now().toUtc(),
+  )();
 }
