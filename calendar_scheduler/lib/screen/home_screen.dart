5a1d614dbd6ad0fe35ef6881dae6aa2b3edd0e3e
boki <lsb530@naver.com>
2025-09-01 22:35:46 +0900

    count tasks - TodayBanner

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -56,9 +56,16 @@ class _HomeScreenState extends State<HomeScreen> {
               onDaySelected: onDaySelected,
               selectedDayPredicate: selectedDayPredicate,
             ),
-            TodayBanner(
-              selectedDay: selectedDay,
-              taskCount: 0,
+            StreamBuilder(
+              stream: GetIt.I<AppDateBase>().streamSchedules(
+                selectedDay,
+              ),
+              builder: (context, snapshot) {
+                return TodayBanner(
+                  selectedDay: selectedDay,
+                  taskCount: !snapshot.hasData ? 0 : snapshot.data!.length,
+                );
+              }
             ),
             Expanded(
               child: Padding(
