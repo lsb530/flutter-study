7c7517a08a98816924c6fd62f05c6c6b246e3f6b
boki <lsb530@naver.com>
2025-09-01 10:32:05 +0900

    insert, select test

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,11 +1,30 @@
-import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:calendar_scheduler/const/color.dart';
+import 'package:drift/drift.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler/database/drift.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = AppDateBase();
+
+  await database.createSchedule(
+    ScheduleTableCompanion(
+      startTime: Value(12),
+      endTime: Value(12),
+      content: Value('Flutter 프로그래밍'),
+      date: Value(DateTime.utc(2025,9,1)),
+      color: Value(categoryColors.first),
+    ),
+  );
+  final resp = await database.getSchedules();
+
+  print('------------------------');
+  print(resp);
 
   runApp(
     MaterialApp(
