aacef8d319738251c8db48a5422f6e085042e989
boki <lsb530@naver.com>
2025-09-02 23:45:32 +0900

    HourlyStat component - multiple, layout

--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dusty_dust/component/category_stat.dart';
+import 'package:dusty_dust/component/hourly_stat.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/color.dart';
 import 'package:flutter/material.dart';
@@ -15,72 +16,7 @@ class HomeScreen extends StatelessWidget {
           children: [
             MainStat(),
             CategoryStat(),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: Card(
-                  color: lightColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      16.0,
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: darkColor,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(16.0),
-                            topRight: Radius.circular(16.0),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 4.0),
-                          child: Text(
-                            '시간별 미세먼지',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 16.0,
-                          vertical: 4.0,
-                        ),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                '11시',
-                              ),
-                            ),
-                            Expanded(
-                              child: Image.asset(
-                                'asset/img/best.png',
-                                height: 20.0,
-                              ),
-                            ),
-                            Expanded(
-                              child: Text(
-                                '보통',
-                                textAlign: TextAlign.right,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            HourlyStat(),
           ],
         ),
       ),
