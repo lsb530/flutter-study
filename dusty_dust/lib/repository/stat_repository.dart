cb45605a529604063e238f6ac666a99f588db4fb
boki <lsb530@naver.com>
2025-09-04 00:13:36 +0900

    convert json to StatModel

--- dusty_dust/lib/repository/stat_repository.dart
+++ dusty_dust/lib/repository/stat_repository.dart
@@ -1,11 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final apiUrl = dotenv.env['API_URL'];
 final serviceKey = dotenv.env['SERVICE_KEY'];
 
 class StatRepository {
-  static Future<Map<String, dynamic>> fetchData() async {
+  static Future<List<StatModel>> fetchData({
+    required ItemCode itemCode,
+  }) async {
+    final itemCodeStr = itemCode == ItemCode.PM25 ? 'PM2.5' : itemCode.name;
+
     final response = await Dio().get(
       '$apiUrl',
       queryParameters: {
@@ -13,14 +18,48 @@ class StatRepository {
         'returnType': 'json',
         'numOfRows': 100,
         'pageNo': 1,
-        'itemCode': 'PM10',
+        'itemCode': itemCodeStr,
         'dataGubun': 'HOUR',
-        'searchCondition': 'WEEK'
-      }
+        'searchCondition': 'WEEK',
+      },
     );
 
-    // print(response.data);
+    // print(response.data['response']);
+    // print(response.data['response']['body']['items']);
 
-    return response.data;
+    final List<String> skipKeys = [
+      'dataGubun',
+      'dataTime',
+      'itemCode',
+    ];
+
+    final rawItemsList = response.data['response']['body']['items'];
+
+    List<StatModel> stats = [];
+
+    for (Map<String, dynamic> item in rawItemsList) {
+      final dateTime = item['dataTime'];
+
+      for (String key in item.keys) {
+        if (skipKeys.contains(key)) {
+          continue;
+        }
+
+        final regionStr = key;
+        final stat = item[key];
+
+        stats = [
+          ...stats,
+          StatModel(
+            region: Region.values.firstWhere((e) => e.name == regionStr),
+            stat: double.parse(stat),
+            dateTime: DateTime.parse(dateTime),
+            itemCode: itemCode,
+          ),
+        ];
+      }
+    }
+
+    return stats;
   }
 }
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:dusty_dust/component/category_stat.dart';
 import 'package:dusty_dust/component/hourly_stat.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/color.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
@@ -14,14 +15,16 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: primaryColor,
       body: SingleChildScrollView(
         child: FutureBuilder(
-          future: StatRepository.fetchData(),
+          future: StatRepository.fetchData(
+            itemCode: ItemCode.PM10,
+          ),
           builder: (context, snapshot) {
-            // print(snapshot.error);
-            // print(snapshot.data);
+            print(snapshot.error);
+            print(snapshot.data);
 
-            if (snapshot.hasData) {
-              print(snapshot.data!['response']['body']['totalCount']);
-            }
+            // if (snapshot.hasData) {
+            //   print(snapshot.data!['response']['body']['totalCount']);
+            // }
             return Column(
               children: [
                 MainStat(),
