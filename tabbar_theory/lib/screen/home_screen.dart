bb2114d8e397a7649f37cbc2853814017d5ec3dc
boki <lsb530@naver.com>
2025-09-04 22:27:15 +0900

    BasicAppbarTabbarScreen

--- tabbar_theory/lib/screen/home_screen.dart
+++ tabbar_theory/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tabbar_theory/screen/basic_appbar_tabbar_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => BasicAppbarTabbarScreen(),
+                  ),
+                );
+              },
+              child: Text('Basic AppBar TabBar Screen'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
