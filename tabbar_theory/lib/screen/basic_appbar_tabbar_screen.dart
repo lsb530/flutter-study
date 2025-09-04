042eafb0012f2131bf7795fce2cb2c5fe4d81a8e
boki <lsb530@naver.com>
2025-09-04 23:05:10 +0900

    Customize basic TabBar - PreferredSize

--- tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -12,37 +12,40 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text('BasicAppBarScreen'),
-          bottom: TabBar(
-            indicatorColor: Colors.red,
-            indicatorWeight: 4.0,
-            // indicatorSize: TabBarIndicatorSize.tab,
-            indicatorSize: TabBarIndicatorSize.label,
-            isScrollable: true,
-            // isScrollable: false, // 갯수가 많아지면 뭉개짐
-            tabAlignment: TabAlignment.center,
-            labelColor: Colors.green,
-            unselectedLabelColor: Colors.grey,
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.w700,
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontWeight: FontWeight.w200,
-            ),
-            tabs: TABS
-                // [
-                //       ...TABS,
-                //       ...TABS,
-                //       ...TABS,
-                //     ]
-                .map(
-                  (e) => Tab(
-                    icon: Icon(e.icon),
-                    child: Text(
-                      e.label,
-                    ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(80),
+            child: TabBar(
+              indicatorColor: Colors.red,
+              indicatorWeight: 4.0,
+              // indicatorSize: TabBarIndicatorSize.tab,
+              indicatorSize: TabBarIndicatorSize.label,
+              isScrollable: true,
+              // isScrollable: false, // 갯수가 많아지면 뭉개짐
+              tabAlignment: TabAlignment.center,
+              labelColor: Colors.green,
+              unselectedLabelColor: Colors.grey,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontWeight: FontWeight.w200,
+              ),
+              tabs: TABS
+              // [
+              //       ...TABS,
+              //       ...TABS,
+              //       ...TABS,
+              //     ]
+                  .map(
+                    (e) => Tab(
+                  icon: Icon(e.icon),
+                  child: Text(
+                    e.label,
                   ),
-                )
-                .toList(),
+                ),
+              )
+                  .toList(),
+            ),
           ),
         ),
         body: TabBarView(
@@ -54,11 +57,11 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
               //       ...TABS,
               //     ]
               .map(
-                    (e) => Center(
-                      child: Icon(e.icon),
-                    ),
-                  )
-                  .toList(),
+                (e) => Center(
+                  child: Icon(e.icon),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
