576bc2cf3fbc210bae18602df2ea086b9c30f7e4
boki <lsb530@naver.com>
2025-09-04 20:52:38 +0900

    HourlyStat sync UI with data

--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             MainStat(region: region),
             CategoryStat(region: region),
-            HourlyStat(),
+            HourlyStat(region: region),
           ],
         ),
       ),
