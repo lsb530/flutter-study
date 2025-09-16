77ec8db401b25d698caf3ba24e6e5f94f8eb9f37
boki <lsb530@naver.com>
2025-09-17 00:54:40 +0900

    AutoDisposeModifier

--- riverpod1/lib/screen/home_screen.dart
+++ riverpod1/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod1/layout/default_layout.dart';
+import 'package:riverpod1/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod1/screen/family_modifier_screen.dart';
 import 'package:riverpod1/screen/future_provider_screen.dart';
 import 'package:riverpod1/screen/state_notifier_provider_screen.dart';
@@ -54,6 +55,14 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => AutoDisposeModifierScreen())
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
           ),
         ],
       ),
