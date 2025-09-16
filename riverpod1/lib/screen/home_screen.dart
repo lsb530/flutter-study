51bd72a2bade3b7419a4235a31377b2c238fbed4
boki <lsb530@naver.com>
2025-09-17 01:09:00 +0900

    ref.listen()

--- riverpod1/lib/screen/home_screen.dart
+++ riverpod1/lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:riverpod1/layout/default_layout.dart';
 import 'package:riverpod1/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod1/screen/family_modifier_screen.dart';
 import 'package:riverpod1/screen/future_provider_screen.dart';
+import 'package:riverpod1/screen/listen_provider_screen.dart';
 import 'package:riverpod1/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod1/screen/state_provider_screen.dart';
 import 'package:riverpod1/screen/stream_provider_screen.dart';
@@ -64,6 +65,14 @@ class HomeScreen extends StatelessWidget {
             },
             child: Text('AutoDisposeModifierScreen'),
           ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => ListenProviderScreen())
+              );
+            },
+            child: Text('ListenProviderScreen'),
+          )
         ],
       ),
     );
