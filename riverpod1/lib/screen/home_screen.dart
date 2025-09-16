bde3d7a8c90392cec1958b753970914fcac306ff
boki <lsb530@naver.com>
2025-09-17 00:48:33 +0900

    FamilyModifier

--- riverpod1/lib/screen/home_screen.dart
+++ riverpod1/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod1/layout/default_layout.dart';
+import 'package:riverpod1/screen/family_modifier_screen.dart';
 import 'package:riverpod1/screen/future_provider_screen.dart';
 import 'package:riverpod1/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod1/screen/state_provider_screen.dart';
@@ -45,7 +46,15 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
-          )
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => FamilyModifierScreen())
+              );
+            },
+            child: Text('FamilyModifierScreen'),
+          ),
         ],
       ),
     );
